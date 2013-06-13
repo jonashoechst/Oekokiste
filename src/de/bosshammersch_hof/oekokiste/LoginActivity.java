3a4604d7cdbed16c87c8f1dc58c96a2b57b8fb6b
Artur Sterz <Artur@swla1604.wlan.uni-marburg.de>
2013-06-13 14:53:44 +0200

    Designchanges

--- src/de/bosshammersch_hof/oekokiste/LoginActivity.java
+++ src/de/bosshammersch_hof/oekokiste/LoginActivity.java
@@ -2,7 +2,10 @@ package de.bosshammersch_hof.oekokiste;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
 
@@ -10,6 +13,19 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		final Button button = (Button) findViewById(R.id.loginButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	View view = null;
+				orderButtonClicked(view);
+            }
+        });
+	}
+	
+	public void orderButtonClicked(View view){
+		Intent intent = new Intent(this, OrderActivity.class);
+		startActivity(intent);
 	}
 
 	@Override
