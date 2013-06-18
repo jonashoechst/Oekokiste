df7d16863b60face0431038cf83a4998fcb648f1
Patrick Lampe <patrick.lampe@googlemail.com>
2013-06-18 16:00:12 +0200

    UpdataArticle but not tested

--- src/de/bosshammersch_hof/oekokiste/postgres/UpdateArticle.java
+++ src/de/bosshammersch_hof/oekokiste/postgres/UpdateArticle.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -11,17 +14,26 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.os.AsyncTask;
-import android.provider.SyncStateContract.Constants;
-import de.bosshammersch_hof.oekokiste.model.Article;
+import de.bosshammersch_hof.oekokiste.Constants;
+import de.bosshammersch_hof.oekokiste.model.*;
 
-public class UpdateArticle extends AsyncTask<Article, Integer, boolean[]> {
+public class UpdateArticle extends AsyncTask<Article, Integer, String[]> {
 
-	public boolean[] doInBackground(Article... params) {
+	public String[] doInBackground(Article... params) {
 		
-		String html;
+		String[] html;
+		LinkedList<String> ll = new LinkedList<String>();
 		
-		try {
-			html = executeHttpGet(Constants.pathToArticleDescription);
+		try{
+			html = executeHttpGet(Constants.pathToArticleDescription + params[0].getId());
+			Pattern pat = Pattern.compile("<img src=*.png");
+			for(String s : html){
+				Matcher m = pat.matcher(s);
+				while(m.find())
+				{
+				    ll.add(m.group());
+				}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -43,18 +55,15 @@ public class UpdateArticle extends AsyncTask<Article, Integer, boolean[]> {
             request.setURI(new URI(url));
             HttpResponse response = client.execute(request);
             in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-            StringBuffer sb = new StringBuffer("");
             String[] lines = null;
             String line = "";
-            String NL = System.getProperty("line.separator");
             int i = 0;
             while ((line = in.readLine()) != null) {
                i++;
                lines[i] = line;
             }
             in.close();
-            String page = sb.toString();
-           return lines;
+            return lines;
             } finally {
             if (in != null) {
                 try {
